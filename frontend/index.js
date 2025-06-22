1878df6a69a67222dc9254650aed72f1551f10f3
aamirjawed <aamirjawed682@gmail.com>
2025-06-22 10:32:02 +0530

    Creating Signup Screen Frontend completed

---
+++ frontend/index.js
@@ -0,0 +1,37 @@
+const form = document.getElementById("form")
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+
+    
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value;
+
+    if(!name || !email || !password){
+        alert("All fields required")
+        return
+    }
+
+    try{
+        const response  = await fetch('http://localhost:3000/users/signup', {
+            method:"POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify({
+                name, 
+                email,
+                password
+            })
+        })
+
+    }catch(err){
+        console.log(err)
+    }
+
+    console.log(name, email, password)
+
+    
+})
